8be86c6941a10cc3c8afb043cd82dbf4a19742a3
mukulzoiko23 <mukul@zoikogroup.com>
2026-07-27 22:04:45 +0530

    Updated Ctas & links

--- components/provider-overview/ProviderOverviewHeroSection.tsx
+++ components/provider-overview/ProviderOverviewHeroSection.tsx
@@ -75,7 +75,7 @@ export default function ProviderOverviewHeroSection() {
             {/* Text link */}
             <Reveal index={4} active={mounted}>
               <Link
-                href="#"
+                href="/availability-signals"
                 className="mt-4 inline-block text-[13px] font-medium transition-opacity hover:opacity-80"
                 style={{ color: ACCENT }}
               >
